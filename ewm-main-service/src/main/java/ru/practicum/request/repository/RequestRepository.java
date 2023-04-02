c8e6de59dc73a608d4de74c223e71e50bffbd1c8
Sergej83 <sergej.d83@yandex.ru>
2023-04-02 18:43:24 +0300

    add: Реализовал функционал запросов на участие в событиях.

--- ewm-main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.request.ParticipationRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RequestRepository extends JpaRepository<ParticipationRequest, Long> {
 
     List<ParticipationRequest> findByEventIdAndIdInOrderById(Long eventId, List<Long> requestIds);
+
+    List<ParticipationRequest> findAllByRequesterId(Long userId);
+
+    Optional<ParticipationRequest> findByEventIdAndRequesterId(Long eventId, Long userId);
 }
---
+++ ewm-main-service/src/main/java/ru/practicum/request/service/RequestServiceImpl.java
@@ -0,0 +1,96 @@
+package ru.practicum.request.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.event.Event;
+import ru.practicum.event.repository.EventRepository;
+import ru.practicum.event.status.EventStatus;
+import ru.practicum.exception.ConflictException;
+import ru.practicum.exception.NotFoundException;
+import ru.practicum.request.ParticipationRequest;
+import ru.practicum.request.dto.ParticipationRequestDto;
+import ru.practicum.request.dto.RequestMapper;
+import ru.practicum.request.repository.RequestRepository;
+import ru.practicum.request.status.ParticipationRequestStatus;
+import ru.practicum.user.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RequestServiceImpl implements RequestService {
+
+
+    private final RequestRepository requestRepository;
+    private final EventRepository eventRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public List<ParticipationRequestDto> getRequests(Long userId) {
+
+        userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException("Пользователь под номером " + userId + " не найден"));
+
+        List<ParticipationRequest> requests = requestRepository.findAllByRequesterId(userId);
+
+        return requests.stream().map(RequestMapper::toRequestDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public ParticipationRequestDto addRequest(Long userId, Long eventId) {
+
+        Optional<ParticipationRequest> optionalRequest = requestRepository.findByEventIdAndRequesterId(eventId, userId);
+        if (optionalRequest.isPresent()) {
+            throw new ConflictException("Запрос номер на участие в событии номер уже существует.");
+        }
+
+        Event event = eventRepository.findById(eventId).orElseThrow(
+                () -> new NotFoundException("Событие под номером " + eventId + " не найдено."));
+
+        if (event.getInitiator().getId().equals(userId)) {
+            throw new ConflictException("Нельзя делать запрос на участие в своём событии.");
+        }
+
+        if (!event.getState().equals(EventStatus.PUBLISHED)) {
+            throw new ConflictException("Событие ещё не опубликовано.");
+        }
+
+        if (event.getConfirmedRequests().size() >= event.getParticipantLimit()) {
+            throw new ConflictException("Лимит участников достигнут");
+        }
+
+        ParticipationRequest request = new ParticipationRequest();
+
+        request.setCreated(LocalDateTime.now());
+        request.setEvent(event);
+        request.setRequesterId(userId);
+        if (event.getRequestModeration()) {
+            request.setStatus(ParticipationRequestStatus.PENDING);
+        } else {
+            request.setStatus(ParticipationRequestStatus.CONFIRMED);
+        }
+
+        return RequestMapper.toRequestDto(requestRepository.save(request));
+    }
+
+    @Override
+    public ParticipationRequestDto cancelRequest(Long userId, Long requestId) {
+
+        ParticipationRequest request = requestRepository.findById(requestId).orElseThrow(
+                () -> new NotFoundException("Запрос не номер " + requestId + " не найден."));
+
+        userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException("Пользователь под номером " + userId + " не найден"));
+
+        if (request.getRequesterId().equals(userId)) {
+            request.setStatus(ParticipationRequestStatus.CANCELED);
+        } else {
+            throw new ConflictException("Можно отклонить только свой запрос.");
+        }
+
+        return RequestMapper.toRequestDto(requestRepository.save(request));
+    }
+}
